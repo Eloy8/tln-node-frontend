bfe7ef376dbe034b6c6325178bca2a9988dad98d
Eloy8 <e.lankheet@hotmail.com>
2019-08-29 16:06:13 +0200

    Refactored main to hooks

--- src/main.js
+++ src/main.js
@@ -1,52 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserService from './services/UserService'
 
-class Main extends React.Component {
-  state = {
-    count: this.props.count || 0,
-    user: null,
-    token: null,
-    profile: null
-  }
+const Main = ({ appCount }) => {
+  const [count, setCount] = useState(appCount || 0);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
-  updateCount = increment => {
-    const { count } = this.state
+  const updateCount = increment => setCount(increment ? count + 1 : count - 1)
 
-    if (increment) {
-      this.setState({
-        count: count + 1
-      })
-    } else {
-      this.setState({
-        count: count - 1
-      })
-    }
-  }
-
-  login = async () => {
+  const login = async () => {
     try {
       return await UserService.login()
       .then((response) => {
-        this.setState({
-          token: response.token,
-          user: response.user
+          setToken(response.token)
+          setUser(response.user)
+          sessionStorage.setItem('token', response.token)     
         })
-        sessionStorage.setItem('token', response.token)     
-      })
-    } catch (e) {
+      } catch (e) {
       console.log(e)
     }
   }
 
-  logout = async () => {
-    const { token } = this.state
+  const logout = async () => {
     try {
       return await UserService.logout(token)
       .then(() => {
-        this.setState({
-          token: null,
-          user: null
-        })
+        setToken(null)
+        setUser(null)
         sessionStorage.removeItem('token')     
       })
     } catch (e) {
@@ -54,60 +35,53 @@ class Main extends React.Component {
     }
   }
 
-  checkLoggedIn = () => {
+  const checkLoggedIn = () => {
     console.log('Logged in?', sessionStorage.getItem('token'))
   }
 
-  fetchProfile = async () => {
-    const { token } = this.state
+  const fetchProfile = async () => {
     try {
     return await UserService.fetchProfile(token)
       .then((response) => {
         console.log('Fetch profile', response)
-        this.setState({
-          profile: response
-        })
+        setProfile(response)
       })
    } catch (e) {
       console.log(e)
     }
   }
 
-  render() {
-    const { token, user, count, profile } = this.state
-
-    if (token) {
-      return(
-        <>
-          <h1>You are logged in now! :D</h1>
-          <button onClick={profile ? () => this.setState({profile: null}) : () => this.fetchProfile()}>Check your profile out!</button>
-          <button onClick={() => this.logout()}>Logout</button>
-          {profile && (
-            <>
-              <h1>Your profile</h1>
-              <p>Name: {profile.name}</p>
-              <p>Age: {profile.age}</p>
-              <p>Email: {profile.email}</p>
-              <p>Created at: {profile.createdAt}</p>
-              <p>Updated at: {profile.updatedAt}</p>
-              <p>ID: {profile._id}</p>
-            </>
-          )}
-        </>
-      );
-    }
-
-    return (
-      <div>
-        <button onClick={() => this.login()}>Login!</button>
-        <button onClick={() => this.checkLoggedIn()}>Check if logged in!</button>
-        {token && <p>{token} test {user.name}</p>}
-        <p>The current count is {count}</p>
-        <button onClick={() => this.updateCount(true)}>Increment</button>
-        <button onClick={() => this.updateCount(false)}>Decrement</button>
-      </div>
-    )
+  if (token) {
+    return(
+      <>
+        <h1>You are logged in now! :D</h1>
+        <button onClick={profile ? () => setProfile(null) : () => fetchProfile()}>Check your profile out!</button>
+        <button onClick={() => logout()}>Logout</button>
+        {profile && (
+          <>
+            <h1>Your profile</h1>
+            <p>Name: {profile.name}</p>
+            <p>Age: {profile.age}</p>
+            <p>Email: {profile.email}</p>
+            <p>Created at: {profile.createdAt}</p>
+            <p>Updated at: {profile.updatedAt}</p>
+            <p>ID: {profile._id}</p>
+          </>
+        )}
+      </>
+    );
   }
+
+  return (
+    <div>
+      <button onClick={() => login()}>Login!</button>
+      <button onClick={() => checkLoggedIn()}>Check if logged in!</button>
+      {token && <p>{token} test {user.name}</p>}
+      <p>The current count is {count}</p>
+      <button onClick={() => updateCount(true)}>Increment</button>
+      <button onClick={() => updateCount(false)}>Decrement</button>
+    </div>
+  )
 }
 
-export default Main
+export default Main;
